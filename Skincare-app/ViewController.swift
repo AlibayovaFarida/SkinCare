ae8fd32830219cc879294acc873c78bae253afac
AlibeyovaFarida <129386607+AlibeyovaFarida@users.noreply.github.com>
2024-08-05 10:48:17 +0400

    first commit

--- Skincare-app/ViewController.swift
+++ Skincare-app/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    private let testLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "Welcome to Skincare✌️✌️"
+        return lb
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
+    
+    private func setupUI(){
+        view.backgroundColor = .systemBackground
+        view.addSubview(testLabel)
+        
+        testLabel.snp.makeConstraints { make in
+            make.center.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
 
 }
 
