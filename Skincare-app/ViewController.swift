8cc76df7a44e459582c53e3b64623437b6fae2bb
AlibeyovaFarida <129386607+AlibeyovaFarida@users.noreply.github.com>
2024-08-05 10:34:24 +0400

    Initial Commit

---
+++ Skincare-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skincare-app
+//
+//  Created by Apple on 05.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
