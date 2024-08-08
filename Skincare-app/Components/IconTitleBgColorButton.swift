b2456de8f9e7bd6d6083eebe8e7abb4795b66cb6
AlibeyovaFarida <129386607+AlibeyovaFarida@users.noreply.github.com>
2024-08-08 16:48:14 +0400

    IconTitleBgColorButton and CustomTextField created

---
+++ Skincare-app/Components/IconTitleBgColorButton.swift
@@ -0,0 +1,118 @@
+//
+//  IconTitleBgColorButton.swift
+//  Skincare-app
+//
+//  Created by Apple on 06.08.24.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class IconTitleBgColorButton: UIView {
+    var action: (() -> Void)?
+    
+    private let bgView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 16
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    private let stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .horizontal
+        sv.spacing = 12
+        sv.alignment = .center
+        return sv
+    }()
+    private let iconImageView: UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    private let titleLabel: UILabel = {
+        let lb = UILabel()
+        lb.font = UIFont(name: "Montserrat-Medium", size: 14)
+        return lb
+    }()
+    
+    init(bgColor: String, icon: String, iconWidth: CGFloat, iconHeight: CGFloat, title: String, titleColor: String){
+        super.init(frame: .zero)
+        self.bgView.backgroundColor = UIColor(named: bgColor)
+        self.iconImageView.image = UIImage(named: icon)
+        self.titleLabel.text = title
+        self.titleLabel.textColor = UIColor(named: titleColor)
+        setupView(iconWidth: iconWidth, iconHeight: iconHeight)
+        setupShadows(bgColor: bgColor)
+        setupGesture()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView(iconWidth: CGFloat, iconHeight: CGFloat){
+        
+        addSubview(bgView)
+        bgView.addSubview(stackView)
+        
+        [
+            iconImageView,
+            titleLabel
+        ].forEach(stackView.addArrangedSubview)
+        
+        bgView.snp.makeConstraints { make in
+            make.width.equalTo(274)
+            make.height.equalTo(44)
+            make.edges.equalToSuperview()
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        iconImageView.snp.makeConstraints { make in
+            make.width.equalTo(iconWidth)
+            make.height.equalTo(iconHeight)
+        }
+    }
+    
+    private func setupShadows(bgColor: String) {
+        guard bgColor == "white" else { return }
+                
+        applyShadow(to: bgView, color: UIColor.black, opacity: 0.04, offset: CGSize(width: 0, height: 0), radius: 1)
+        applyShadow(to: bgView, color: UIColor.black, opacity: 0.04, offset: CGSize(width: 0, height: 2), radius: 6)
+        applyShadow(to: bgView, color: UIColor.black, opacity: 0.05, offset: CGSize(width: 0, height: 16), radius: 24)
+    }
+    
+    private func applyShadow(to view: UIView, color: UIColor, opacity: Float, offset: CGSize, radius: CGFloat) {
+        view.layer.shadowColor = color.cgColor
+        view.layer.shadowOpacity = opacity
+        view.layer.shadowOffset = offset
+        view.layer.shadowRadius = radius
+        view.layer.masksToBounds = false
+    }
+    
+    private func setupGesture(){
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        longPressGesture.minimumPressDuration = 0
+        bgView.addGestureRecognizer(longPressGesture)
+    }
+    
+    @objc private func handleLongPress(_ gesture: UILongPressGestureRecognizer) {
+        switch gesture.state {
+            case .began:
+                animate(scale: 0.95)
+            case .ended, .cancelled:
+                animate(scale: 1.0)
+                action?()
+            default:
+                break
+            }
+        }
+        
+    private func animate(scale: CGFloat) {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.bgView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        })
+    }
+}
--- Skincare-app/Screens/Test/ViewController.swift
+++ Skincare-app/Screens/Test/ViewController.swift
@@ -9,25 +9,112 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    private let stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .vertical
+        sv.spacing = 8
+        return sv
+    }()
     private let testLabel: UILabel = {
         let lb = UILabel()
         lb.text = NSLocalizedString("test", comment: "")
-        lb.font = UIFont(name: "Sacramento-Regular", size: 36)
+        lb.font = UIFont(name: "Sacramento-Regular", size: 30)
+        lb.numberOfLines = 0
         return lb
     }()
+    
+    private let appleButton = IconTitleBgColorButton(bgColor: "black", icon: "apple-icon", iconWidth: 18, iconHeight: 22.5, title: NSLocalizedString("appleButtonLoginTitle", comment: ""), titleColor: "white")
+    
+    private let googleButton = IconTitleBgColorButton(bgColor: "white", icon: "google-icon", iconWidth: 18, iconHeight: 18, title: NSLocalizedString("googleButtonLoginTitle", comment: ""), titleColor: "black")
+    
+    private let mailButton = IconTitleBgColorButton(bgColor: "white", icon: "mail-icon", iconWidth: 17.18, iconHeight: 14.7, title: NSLocalizedString("mailButtonLoginTitle", comment: ""), titleColor: "black")
+    
+    private let emailTextField = CustomTextField(placeholder: "Email")
+    
+    private let passwordTextField = CustomTextField(placeholder: "Password")
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        setupActions()
     }
     
     private func setupUI(){
         view.backgroundColor = .systemBackground
-        view.addSubview(testLabel)
-        
-        testLabel.snp.makeConstraints { make in
+
+        view.addSubview(stackView)
+        [
+            testLabel,
+            appleButton,
+            googleButton,
+            mailButton,
+            emailTextField,
+            passwordTextField
+        ].forEach(stackView.addArrangedSubview)
+        stackView.snp.makeConstraints { make in
             make.center.equalTo(view.safeAreaLayoutGuide)
         }
     }
-
+    
+    private func setupActions(){
+        
+        appleButton.action = {
+            print("Apple 🍎")
+        }
+        
+        googleButton.action = {
+            print("Google 🔍")
+        }
+        
+        mailButton.action = {
+            print("Mail ✉️")
+        }
+        
+        emailTextField.textField.addTarget(self, action: #selector(didTapEmailValidate), for: .editingChanged)
+        
+        passwordTextField.textField.addTarget(self, action: #selector(didTapPasswordValidate), for: .editingChanged)
+    }
+    
+    @objc
+    private func didTapEmailValidate(){
+        if !isValidEmail(email: emailTextField.textField.text ?? "" )  {
+            UIView.animate(withDuration: 0.2) { [self] in
+                inValidState(textField: emailTextField, errorMessage: "Invalid email")
+            }
+        } else{
+            validState(textField: emailTextField)
+        }
+    }
+    @objc
+    private func didTapPasswordValidate(){
+        if !isValidPassword(password: passwordTextField.textField.text ?? ""){
+            UIView.animate(withDuration: 0.2) { [self] in
+                self.inValidState(textField: passwordTextField, errorMessage: "Invalid password")
+            }
+        } else {
+            validState(textField: passwordTextField)
+        }
+    }
+    func isValidEmail(email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+    func isValidPassword(password: String) -> Bool {
+        let passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]{8,}$"
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        return passwordPredicate.evaluate(with: password)
+    }
+    func inValidState(textField: CustomTextField, errorMessage: String){
+        textField.errorLabel.isHidden = false
+        textField.errorLabel.text = errorMessage
+        textField.textFieldView.layer.borderWidth = 1
+        textField.textFieldView.layer.borderColor = .init(red: 255, green: 0, blue: 0, alpha: 1)
+    }
+    func validState(textField: CustomTextField){
+        textField.errorLabel.isHidden = true
+        textField.errorLabel.text = ""
+        textField.textFieldView.layer.borderWidth = 1
+        textField.textFieldView.layer.borderColor = .init(red: 255, green: 255, blue: 255, alpha: 1)
+    }
 }
 
