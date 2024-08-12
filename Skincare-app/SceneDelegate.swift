896c2ccb5a01b2fd984f97765be211ed60450b8c
Umman Babayev <ummanbabayev1@gmail.com>
2024-08-12 20:52:53 +0400

    Small issues fixed

--- Skincare-app/SceneDelegate.swift
+++ Skincare-app/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene);
-        let navVC = UINavigationController(rootViewController: CompleteTestViewController());
+        let navVC = UINavigationController(rootViewController: LoginViewController());
         window?.rootViewController = navVC;
         window?.makeKeyAndVisible();
     }
